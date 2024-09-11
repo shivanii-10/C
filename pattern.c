acad664975dedf24948cdceede46a354fe26db1e
shivanii-10 <shivanianushka144@gmail.com>
2024-09-12 03:20:56 +0530

    pattern for hollow inverted half pyramid

---
+++ pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--) {
+        
+        for (j = 1; j <= i; j++) {
+           
+            if (j == 1 || j == i || i == n) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+       
+        printf("\n");
+    }
+
+    return 0;
+}
